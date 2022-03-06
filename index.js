ab3be21a0a89ce7f11903569cfd77b535131f3b7
Arkadz <penkratpavel@gmail.com>
2022-03-06 16:48:54 +0300

    feat: add js and style

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav');
+const navList = document.querySelector('.nav-list');
+const hamburger = document.querySelector('.hamburger');
+const lines = document.querySelectorAll('.line');
+
+
 
 const container = document.querySelector('.container');
 
 burger.addEventListener('click', ()=>{
     nav.classList.toggle('closed');
+    lines.forEach(item=>{
+        item.classList.toggle('open');
+    });
 });
 
-let data = [];
+
+nav.addEventListener('click',(event)=>{
+    if (event.target.classList.contains('nav-link')){
+        nav.classList.toggle('closed');
+        lines.forEach(item=>{
+            item.classList.toggle('open');
+        });             
+
+    }
+})
+
+
 
 function addText(data) {
     data.forEach(item => {
         
         const chapter = document.createElement('div');
         chapter.classList.add('chapter');
+        chapter.id = item['href'];
         container.appendChild(chapter);
         
         const header = document.createElement('div');
@@ -32,6 +52,24 @@ function addText(data) {
     });
 }
 
+function addNavList(data) {
+    data.forEach(item=>{
+        const navItem = document.createElement('li');
+        navItem.classList.add('nav-item');
+        
+        const navLink = document.createElement('a');
+        navLink.classList.add('nav-link');
+        navLink.href = `#${item['href']}`;
+        navLink.innerText = item['nav'];
+        navItem.appendChild(navLink);
+
+
+        navList.appendChild(navItem);
+
+    });
+}
+
+
 
 async function getText() {
     try {
@@ -39,6 +77,7 @@ async function getText() {
         const data = await res.json();
         console.log(data.length);
         addText(data);
+        addNavList(data);
 
 
     } catch(error) {
